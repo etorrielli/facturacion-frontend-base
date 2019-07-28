1fc4aae2baf573f3f7948c362143a281a3fc9dbc
edgar <e.torrielli@gmail.com>
2019-07-28 20:43:20 -0300

    first commit

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <span><a href="http://coreui.io"></a></span>
+    <span class="ml-auto">Powered by <a href="https://www.ekeepoit.com">eKeepOIT</a></span>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'c-footer'
+}
+</script>
